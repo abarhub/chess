a52106c2b3f5eab1de459d9f63f4c791fd1fdd4b
toto963741 <tulipebleu1@laposte.net>
2018-03-31 13:08:29 +0200

    affichage des positions possibles des pieces sur le tableau

--- src/main/resources/static/js/chess2.js
+++ src/main/resources/static/js/chess2.js
@@ -125,11 +125,141 @@ function logInfos() {
     });
 }
 
+var removeGreySquares = function () {
+    $('#myBoard .square-55d63').css('background', '');
+};
+
+var greySquare = function (square) {
+    var squareEl = $('#myBoard .square-' + square);
+
+    var background = '#a9a9a9';
+    if (squareEl.hasClass('black-3c85d') === true) {
+        background = '#696969';
+    }
+
+    squareEl.css('background', background);
+};
+
+var onDragStart = function (source, piece) {
+    // // do not pick up pieces if the game is over
+    // // or if it's not that side's turn
+    // if (game.game_over() === true ||
+    //         (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+    //         (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+    //     return false;
+    // }
+};
+
+var onDrop = function (source, target) {
+    // removeGreySquares();
+    //
+    // // see if the move is legal
+    // var move = game.move({
+    //     from: source,
+    //     to: target,
+    //     promotion: 'q' // NOTE: always promote to a queen for example simplicity
+    // });
+    //
+    // // illegal move
+    // if (move === null) return 'snapback';
+};
+
+var getMoves = function (ligne, colonne, success) {
+    console.log("getMoves", ligne, colonne);
+    $.ajax({
+        type: "GET",
+        //dataType: 'json',
+        url: "http://localhost:8080/deplacements/" + ligne + "/" + colonne,
+        success: function (data) {
+            console.log("deplacements : data", data);
+
+            //loadPieces();
+            if (success) {
+                success(data);
+            }
+        },
+        error: function (resultat, statut, erreur) {
+            console.error("deplacements : error", resultat, statut, erreur);
+
+        }
+    });
+};
+
+// var getMoves2 = function (position) {
+//     getMoves()
+// };
+
+var onMouseoverSquare = function (square, piece) {
+    console.log("onMouseoverSquare", square, piece);
+    // get list of possible moves for this square
+    // var moves = game.moves({
+    //     square: square,
+    //     verbose: true
+    // });
+    var moves = [''];
+
+    if (!!square && square.length == 2) {
+
+        console.log("charAt", square.charAt(0), square.charAt(1));
+
+        var colonne = square.charCodeAt(0) - 97;
+        var ligne = square.charCodeAt(1) - 49;
+        getMoves(ligne, colonne, function (data) {
+
+            if (data.length == 0) return;
+
+            // exit if there are no moves available for this square
+            //if (moves.length === 0) return;
+
+            // highlight the square they moused over
+            greySquare(square);
+
+            // highlight the possible squares for this piece
+            for (var i = 0; i < data.length; i++) {
+                var ligne2 = data[i].ligne;
+                var colonne2 = data[i].colonne;
+                var pos = String.fromCharCode(colonne2 + 97) + String.fromCharCode(ligne2 + 49);
+                greySquare(pos);
+            }
+        });
+
+        // // exit if there are no moves available for this square
+        // if (moves.length === 0) return;
+        //
+        // // highlight the square they moused over
+        // greySquare(square);
+
+        // highlight the possible squares for this piece
+        // for (var i = 0; i < moves.length; i++) {
+        //     greySquare(moves[i].to);
+        // }
+    }
+
+};
+
+var onMouseoutSquare = function (square, piece) {
+    removeGreySquares();
+};
+
+var onSnapEnd = function () {
+    //board.position(game.fen());
+};
+
 function init() {
     //alert('reload cart called');
 
+    var cfg = {
+        //draggable: true,
+        //position: 'start',
+        //onDragStart: onDragStart,
+        //onDrop: onDrop,
+        onMouseoutSquare: onMouseoutSquare,
+        onMouseoverSquare: onMouseoverSquare//,
+        //onSnapEnd: onSnapEnd
+    };
+
     // board = Chessboard('#myBoard', 'start');
-    board = Chessboard('#myBoard');
+    board = Chessboard('#myBoard', cfg);
 
     // $('#startPositionBtn').on('click', function () {
     //     //board.position('r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R');
